1f3794371cfc16986ceb904f274f1a4d8203b515
samrapeer <samrapeerbaksh@gmail.com>
2025-01-16 02:50:29 +0500

    update the developer names

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className='bg-slate-200'>
       <div className='container mx-auto p-4'>
        <p className='text-center text-2xl font-bold'>Tech Heaven</p>
-       <p className='text-center mt-2'>Made with ❤️ by Samra</p>
+       <p className='text-center mt-2'>Made with ❤️ by Samar & Suhaima</p>
       </div>
     </footer>
   )
